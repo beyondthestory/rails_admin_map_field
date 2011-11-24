1f4b4dac7f178a33973ccd93e29fbd54bc739b78
Jason Langenauer <jason@jasonlangenauer.com>
2011-11-24 13:59:07 +0000

    Display a google map centered on the location

--- lib/rails_admin_map_field/rails_admin/config/fields/types/map.rb
+++ lib/rails_admin_map_field/rails_admin/config/fields/types/map.rb
@@ -9,5 +9,21 @@ module RailsAdmin::Config::Fields::Types
     register_instance_option(:partial) do
       :form_map
     end
+
+    register_instance_option(:google_api_key) do
+      nil
+    end
+
+    register_instance_option(:default_latitude) do
+      51.5 # Latitude of London, United Kingdom
+    end
+
+    register_instance_option(:default_longitude) do
+      -0.5 # Longitude of London, United Kingdom
+    end
+
+    def dom_name
+      @dom_name ||= "#{bindings[:form].object_name}_#{@name}_#{@longitude_name}"
+    end
   end
 end
