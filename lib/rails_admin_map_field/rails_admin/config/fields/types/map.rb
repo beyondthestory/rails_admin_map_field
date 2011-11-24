8798ae2b56d6a8348e155f60ee8a268c8f167e0f
Jason Langenauer <jason@jasonlangenauer.com>
2011-11-24 15:00:46 +0000

    Add readme
    Readme fixes
    
    Remove email address
    
    Remove reference to unused ramf.js
    
    Add parameter for zoom level
    
    adding fields in lib
    
    geocode the address and use that to set the lat/lng
    
    dom methods
    
    parenthesis
    
    trying strings instead
    
    trying without globals?
    
    fix geocoder and bump version number
    
    update readme
    
    updating readme
    
    dom methods
    
    parenthesis
    
    trying strings instead
    
    trying without globals?
    
    fix geocoder and bump version number
    
    update readme
    
    make using country and state fields optional
    
    add map_width and map_height, and run map code in gmap load callback
    
    Fix if address is undefined
    
    Add back listener to watch for map clicks
    
    Add lat and lon live textfields
    
    Update README
    
    Revert to old commit - hopefully it works
    
    Save and display data on form submission
    
    Add readme
    
    Readme fixes
    
    Remove email address
    
    Remove reference to unused ramf.js
    
    Add parameter for zoom level
    
    adding fields in lib
    
    geocode the address and use that to set the lat/lng
    
    dom methods
    
    parenthesis
    
    trying strings instead
    
    trying without globals?
    
    fix geocoder and bump version number
    
    update readme
    
    updating readme
    
    dom methods
    
    parenthesis
    
    trying strings instead
    
    trying without globals?
    
    fix geocoder and bump version number
    
    update readme
    
    make using country and state fields optional
    
    Save and display data on form submission
    
    Add readme
    
    Readme fixes
    
    Remove email address
    
    Remove reference to unused ramf.js
    
    Add parameter for zoom level
    
    adding fields in lib
    
    geocode the address and use that to set the lat/lng
    
    dom methods
    
    parenthesis
    
    trying strings instead
    
    trying without globals?
    
    fix geocoder and bump version number
    
    update readme
    
    updating readme
    
    dom methods
    
    parenthesis
    
    trying strings instead
    
    trying without globals?
    
    fix geocoder and bump version number
    
    update readme
    
    make using country and state fields optional
    
    add map_width and map_height, and run map code in gmap load callback
    
    Fix if address is undefined
    
    Add back listener to watch for map clicks
    
    Add lat and lon live textfields
    
    Update README
    
    Update README
    
    Update README
    
    Update README
    
    Save and display data on form submission
    
    Add readme
    
    Readme fixes
    
    Remove email address
    
    Remove reference to unused ramf.js
    
    Add parameter for zoom level
    
    adding fields in lib
    
    geocode the address and use that to set the lat/lng
    
    dom methods
    
    parenthesis
    
    trying strings instead
    
    trying without globals?
    
    fix geocoder and bump version number
    
    update readme
    
    updating readme
    
    dom methods
    
    parenthesis
    
    trying strings instead
    
    trying without globals?
    
    fix geocoder and bump version number
    
    update readme
    
    make using country and state fields optional
    
    add map_width and map_height, and run map code in gmap load callback
    
    Fix if address is undefined
    
    Add back listener to watch for map clicks
    
    Add lat and lon live textfields
    
    Update README
    
    Update README
    
    Update README
    
    Conflicts:
    	README.md
    
    Fucked up repo - remove merge rubbish
    
    Save and display data on form submission
    
    Add readme
    
    Readme fixes
    
    Remove email address
    
    Remove reference to unused ramf.js
    
    Add parameter for zoom level
    
    adding fields in lib
    
    geocode the address and use that to set the lat/lng
    
    dom methods
    
    parenthesis
    
    trying strings instead
    
    trying without globals?
    
    fix geocoder and bump version number
    
    update readme
    
    updating readme
    
    dom methods
    
    parenthesis
    
    trying strings instead
    
    trying without globals?
    
    fix geocoder and bump version number
    
    update readme
    
    make using country and state fields optional
    
    add map_width and map_height, and run map code in gmap load callback
    
    Fix if address is undefined
    
    Add back listener to watch for map clicks
    
    Add lat and lon live textfields
    
    Update README
    
    Update README
    
    Update README
    
    Conflicts:
    	README.md
    
    Remove all file
    
    pull request readmy updates

--- lib/rails_admin_map_field/rails_admin/config/fields/types/map.rb
+++ lib/rails_admin_map_field/rails_admin/config/fields/types/map.rb
@@ -2,10 +2,26 @@ module RailsAdmin::Config::Fields::Types
   class Map < RailsAdmin::Config::Fields::Base
     RailsAdmin::Config::Fields::Types::register(:map, self)
 
+    def allowed_methods
+      [@name, longitude_field]
+    end
+
     # THe name of the corresponding longitude field to match the latitude field
     # in this object.
     register_instance_option(:longitude_field) do
-      :longitude
+      "longitude"
+    end
+
+    register_instance_option(:address_field) do
+      "address"
+    end
+
+    register_instance_option(:city_field) do
+      "city"
+    end
+
+    register_instance_option(:state_field) do
+      "state"
     end
 
     register_instance_option(:partial) do
@@ -20,24 +36,52 @@ module RailsAdmin::Config::Fields::Types
     # Latitude value to display in the map if the latitude attribute is nil
     # (Otherwise the location defaults to (0,0) which is in the Gulf of Guinea
     register_instance_option(:default_latitude) do
-      51.5 # Latitude of London, United Kingdom
+      40.711417 # Latitude of Jersey City, NJ
     end
 
     # Longitude value to display if the longitude attribute is nil
     register_instance_option(:default_longitude) do
-      -0.126 # Longitude of London, United Kingdom
+      74.0647 # Longitude of Jersey City, NJ
     end
 
+    # Default zoom level of the map
+    register_instance_option(:default_zoom_level) do
+      8
+    end
+
+    # Google Maps API Key - optional
+    register_instance_option(:map_width) do
+      "300px"
+    end
+
+    # Google Maps API Key - optional
+    register_instance_option(:map_height) do
+      "200px"
+    end
+
+
     def dom_name
-      @dom_name ||= "#{bindings[:form].object_name}_#{@name}_#{@longitude_field}"
+      @dom_name ||= "#{bindings[:form].object_name}_#{@name}_#{longitude_field}"
     end
 
     def latitude_dom_name
-      @lat_dom_name ||= "#{bindings[:form].object_name}_#{@name}"
+      "#{bindings[:form].object_name}_#{@name}"
     end
 
     def longitude_dom_name
-      @lon_dom_name ||= "#{bindings[:form].object_name}_#{@longitude_field}"
+      @lon_dom_name ||= "#{bindings[:form].object_name}_#{longitude_field}"
+    end
+
+    def address_dom_name
+      "#{bindings[:form].object_name}_#{address_field}"
+    end
+
+    def city_dom_name
+      "#{bindings[:form].object_name}_#{city_field}"
+    end
+
+    def state_dom_name
+      "#{bindings[:form].object_name}_#{state_field}"
     end
   end
 end
