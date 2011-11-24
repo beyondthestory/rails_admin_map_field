abee058837448503c9cd7058a55df802b2571819
Jason Langenauer <jason@jasonlangenauer.com>
2011-11-24 14:43:48 +0000

    Save and display data on form submission

--- lib/rails_admin_map_field/rails_admin/config/fields/types/map.rb
+++ lib/rails_admin_map_field/rails_admin/config/fields/types/map.rb
@@ -2,6 +2,8 @@ module RailsAdmin::Config::Fields::Types
   class Map < RailsAdmin::Config::Fields::Base
     RailsAdmin::Config::Fields::Types::register(:map, self)
 
+    # THe name of the corresponding longitude field to match the latitude field
+    # in this object.
     register_instance_option(:longitude_field) do
       :longitude
     end
@@ -10,20 +12,32 @@ module RailsAdmin::Config::Fields::Types
       :form_map
     end
 
+    # Google Maps API Key - optional
     register_instance_option(:google_api_key) do
       nil
     end
 
+    # Latitude value to display in the map if the latitude attribute is nil
+    # (Otherwise the location defaults to (0,0) which is in the Gulf of Guinea
     register_instance_option(:default_latitude) do
       51.5 # Latitude of London, United Kingdom
     end
 
+    # Longitude value to display if the longitude attribute is nil
     register_instance_option(:default_longitude) do
-      -0.5 # Longitude of London, United Kingdom
+      -0.126 # Longitude of London, United Kingdom
     end
 
     def dom_name
-      @dom_name ||= "#{bindings[:form].object_name}_#{@name}_#{@longitude_name}"
+      @dom_name ||= "#{bindings[:form].object_name}_#{@name}_#{@longitude_field}"
+    end
+
+    def latitude_dom_name
+      @lat_dom_name ||= "#{bindings[:form].object_name}_#{@name}"
+    end
+
+    def longitude_dom_name
+      @lon_dom_name ||= "#{bindings[:form].object_name}_#{@longitude_field}"
     end
   end
 end
