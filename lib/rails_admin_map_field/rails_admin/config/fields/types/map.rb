b53102c4c1d1be3a50609d70bf77971c39e90273
Bartłomiej Skwira <bartlomiej.skwira@amberbit.com>
2013-11-15 13:47:57 +0100

    Save and display data on form submission
    Add readme
    
    Readme fixes
    
    Remove email address
    
    Remove reference to unused ramf.js
    
    Add parameter for zoom level
    
    adding fields in lib
    
    geocode the address and use that to set the lat/lng
    
    dom methods
    
    parenthesis
    
    trying strings instead
    
    trying without globals?
    
    fix geocoder and bump version number
    
    update readme
    
    updating readme
    
    dom methods
    
    parenthesis
    
    trying strings instead
    
    trying without globals?
    
    fix geocoder and bump version number
    
    update readme
    
    make using country and state fields optional
    
    add map_width and map_height, and run map code in gmap load callback
    
    Fix if address is undefined
    
    Add back listener to watch for map clicks
    
    Add lat and lon live textfields
    
    Update README
    
    Update README
    
    Update README
    
    Update README

--- lib/rails_admin_map_field/rails_admin/config/fields/types/map.rb
+++ lib/rails_admin_map_field/rails_admin/config/fields/types/map.rb
@@ -2,6 +2,10 @@ module RailsAdmin::Config::Fields::Types
   class Map < RailsAdmin::Config::Fields::Base
     RailsAdmin::Config::Fields::Types::register(:map, self)
 
+    def allowed_methods
+      [@name, longitude_field]
+    end
+
     # THe name of the corresponding longitude field to match the latitude field
     # in this object.
     register_instance_option(:longitude_field) do
@@ -45,8 +49,19 @@ module RailsAdmin::Config::Fields::Types
       8
     end
 
+    # Google Maps API Key - optional
+    register_instance_option(:map_width) do
+      "300px"
+    end
+
+    # Google Maps API Key - optional
+    register_instance_option(:map_height) do
+      "200px"
+    end
+
+
     def dom_name
-      "#{bindings[:form].object_name}_#{@name}_#{longitude_field}"
+      @dom_name ||= "#{bindings[:form].object_name}_#{@name}_#{longitude_field}"
     end
 
     def latitude_dom_name
@@ -54,6 +69,7 @@ module RailsAdmin::Config::Fields::Types
     end
 
     def longitude_dom_name
+<<<<<<< HEAD
       "#{bindings[:form].object_name}_#{longitude_field}"
 <<<<<<< HEAD
     end
@@ -70,6 +86,9 @@ module RailsAdmin::Config::Fields::Types
       "#{bindings[:form].object_name}_#{state_field}"
 =======
 >>>>>>> 1710eaa... trying without globals?
+=======
+      @lon_dom_name ||= "#{bindings[:form].object_name}_#{longitude_field}"
+>>>>>>> 16a352b... add map_width and map_height, and run map code in gmap load callback
     end
 
     def address_dom_name
